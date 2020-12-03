aa90e58f37e42cb5a410174945439a4560e3f031
VF2048 <48253355+VF2048@users.noreply.github.com>
2020-12-03 16:36:17 +0300

    add sort

--- src/App.js
+++ src/App.js
@@ -1,39 +1,6 @@
 import React from 'react';
 import './App.css';
-
-function Table(props) {
-  let list = props.data.map((elem) => {
-    return (
-      <tr key={elem.id}>
-        <th>{elem.id}</th>
-        <th>{elem.firstName}</th>
-        <th>{elem.lastName}</th>
-        <th>{`${elem.address.streetAddress},${elem.address.city},${elem.address.state},${elem.address.zip}`}</th>
-        <th>{elem.email}</th>
-        <th>{elem.phone}</th>
-        <th>{elem.description}</th>
-      </tr>
-    )
-  })
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>firstName</th>
-          <th>lastName</th>
-          <th>address</th>
-          <th>email</th>
-          <th>phone</th>
-          <th>description</th>
-        </tr>
-      </thead>
-      <tbody>
-        {list}
-      </tbody>
-    </table>
-  )
-}
+import Table from './Table';
 
 class App extends React.Component {
   constructor(props) {
@@ -42,21 +9,23 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch("http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}")
-      .then(res => res.json())
-      .then(json => this.setState({ data: json }))
+    fetch(
+      'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',
+    )
+      .then((res) => res.json())
+      .then((json) => this.setState({ data: json }));
   }
 
   render() {
-    if (this.state.data)
+    const { data } = this.state;
+    if (data) {
       return (
         <div>
-          <Table data={this.state.data} />
+          <Table data={data} />
         </div>
-      )
-    else
-      return (<div></div>)
+      );
+    }
+    return <div />;
   }
 }
-
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nav: 'AAA',
+      navigate: {
+        id: '▲',
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+      },
+    };
+  }
+
+  componentDidMount() {
+    this.list();
+    this.sorting('id');
+  }
+
+  list() {
+    const { data } = this.props;
+    this.setState({
+      list: data.map((elem) => (
+        <tr key={elem.id}>
+          <th>{elem.id}</th>
+          <th>{elem.firstName}</th>
+          <th>{elem.lastName}</th>
+          <th>{elem.email}</th>
+          <th>{elem.phone}</th>
+          {/* <th>{`${elem.address.streetAddress},
+                ${elem.address.city},
+                ${elem.address.state},
+                ${elem.address.zip}`}</th>
+            <th>{elem.description}</th> */}
+        </tr>
+      )),
+    });
+  }
+
+  sorting(elem) {
+    const { data } = this.props;
+    let { nav } = this.state;
+    const { navigate } = this.state;
+
+    if (navigate[elem] !== '') {
+      if (navigate[elem] === '▲') navigate[elem] = '▼';
+      else navigate[elem] = '▲';
+    } else {
+      navigate[nav] = '';
+      navigate[elem] = '▲';
+    }
+
+    if (elem === nav) {
+      data.reverse();
+    } else if (elem === 'id') {
+      data.sort((a, b) => a.id - b.id);
+      nav = 'id';
+    } else if (elem === 'phone') {
+      data.sort((a, b) => {
+        const phoneA = parseInt(`${a.phone}`.replace(/\D/g, ''), 10);
+        const phoneB = parseInt(`${b.phone}`.replace(/\D/g, ''), 10);
+        return phoneA - phoneB;
+      });
+    } else {
+      data.sort((a, b) => a[elem].localeCompare(b[elem]));
+    }
+    this.setState({ nav: elem, navigate });
+    this.list();
+  }
+
+  render() {
+    const { list } = this.state;
+    const { navigate } = this.state;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th className="head" onClick={() => this.sorting('id')}>
+              id
+              {navigate.id}
+            </th>
+            <th className="head" onClick={() => this.sorting('firstName')}>
+              firstName
+              {navigate.firstName}
+            </th>
+            <th className="head" onClick={() => this.sorting('lastName')}>
+              lastName
+              {navigate.lastName}
+            </th>
+            <th className="head" onClick={() => this.sorting('email')}>
+              email
+              {navigate.email}
+            </th>
+            <th className="head" onClick={() => this.sorting('phone')}>
+              phone
+              {navigate.phone}
+            </th>
+            {/* <th>address</th>
+            <th>description</th> */}
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+    );
+  }
+}
+
+Table.defaultProps = { data: [] };
+
+Table.propTypes = {
+  data: PropTypes.objectOf(),
+};
+
+export default Table;
